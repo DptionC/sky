13a10a81f26bf584c36709420c50a9e7d90efc54
DptionC <991703988@qq.com>
2024-04-24 15:13:18 +0800

    新增员工业务代码开发

--- sky-common/src/main/java/com/sky/context/BaseContext.java
+++ sky-common/src/main/java/com/sky/context/BaseContext.java
@@ -2,16 +2,20 @@ package com.sky.context;
 
 public class BaseContext {
 
+    //创建一个长整型的ThreadLocal集合对象
     public static ThreadLocal<Long> threadLocal = new ThreadLocal<>();
 
+    //创建一个设置当前线程的线程局部变量的值
     public static void setCurrentId(Long id) {
         threadLocal.set(id);
     }
 
+    //创建一个返回当前线程所对应的线程局部变量的值
     public static Long getCurrentId() {
         return threadLocal.get();
     }
 
+    //创建一个移除当前线程的线程局部变量
     public static void removeCurrentId() {
         threadLocal.remove();
     }
--- sky-server/src/main/java/com/sky/handler/GlobalExceptionHandler.java
+++ sky-server/src/main/java/com/sky/handler/GlobalExceptionHandler.java
@@ -1,10 +1,13 @@
 package com.sky.handler;
 
+import com.sky.constant.MessageConstant;
 import com.sky.exception.BaseException;
 import com.sky.result.Result;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.sql.SQLIntegrityConstraintViolationException;
 
 /**
  * 全局异常处理器，处理项目中抛出的业务异常
@@ -22,6 +25,26 @@ public class GlobalExceptionHandler {
     public Result exceptionHandler(BaseException ex){
         log.error("异常信息：{}", ex.getMessage());
         return Result.error(ex.getMessage());
+    }
+
+    @ExceptionHandler
+    public Result exceptionHandler(SQLIntegrityConstraintViolationException ex) {
+        //Duplicate entry 'zhangsan' for key 'employee.idx_username',使用getMessage捕获错误信息
+        String message = ex.getMessage();
+        //判断是否包含键值对
+        if (message.contains("Duplicate entry")) {
+            //如果包含,则通过空格分割
+            String[] split = message.split(" ");
+            //根据分割后的索引下标获取重复的用户名
+            String username = split[2];
+            //进行字符串拼接
+            String msg = username + MessageConstant.ALREADY_EXISTS;
+            //封装结果返回
+            return Result.error(msg);
+        }else{
+            //如果是其他问题,则返回未知错误
+            return Result.error(MessageConstant.UNKNOWN_ERROR);
+        }
     }
 
 }
