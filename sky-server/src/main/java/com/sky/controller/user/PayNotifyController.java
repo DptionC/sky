39f76f041061121fbb184d3cb40db765c2883eff
DptionC <991703988@qq.com>
2024-05-02 13:43:29 +0800

    订单支付业务功能代码开发

---
+++ sky-server/src/main/java/com/sky/controller/user/PayNotifyController.java
@@ -0,0 +1,131 @@
+package com.sky.controller.user;
+
+import com.alibaba.druid.support.json.JSONUtils;
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONObject;
+import com.sky.properties.WeChatProperties;
+import com.sky.service.OrderService;
+import com.wechat.pay.contrib.apache.httpclient.util.AesUtil;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.http.entity.ContentType;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.BufferedReader;
+import java.nio.charset.StandardCharsets;
+import java.util.HashMap;
+
+/**
+ * 支付回调相关接口
+ */
+@RestController
+@RequestMapping("/notify")
+@Slf4j
+public class PayNotifyController {
+    @Autowired
+    private OrderService orderService;
+    @Autowired
+    private WeChatProperties weChatProperties;
+
+    /**
+     * 支付成功回调
+     *
+     * @param request
+     */
+    @RequestMapping("/paySuccess")
+    public void paySuccessNotify(HttpServletRequest request, HttpServletResponse response) throws Exception {
+        //读取数据
+        String body = readData(request);
+        log.info("支付成功回调：{}", body);
+
+        //数据解密
+        String plainText = decryptData(body);
+        log.info("解密后的文本：{}", plainText);
+
+        JSONObject jsonObject = JSON.parseObject(plainText);
+        String outTradeNo = jsonObject.getString("out_trade_no");//商户平台订单号
+        String transactionId = jsonObject.getString("transaction_id");//微信支付交易号
+
+        log.info("商户平台订单号：{}", outTradeNo);
+        log.info("微信支付交易号：{}", transactionId);
+
+        //业务处理，修改订单状态、来单提醒
+        orderService.paySuccess(outTradeNo);
+
+        //给微信响应
+        responseToWeixin(response);
+    }
+
+    /**
+     * 定义读取数据方法
+     * @param request
+     * @return
+     * @throws Exception
+     */
+    private String readData(HttpServletRequest request) throws Exception {
+        //获取请求体内容,并将其转换成字符串
+        BufferedReader reader = request.getReader();
+        //创建非线程安全可变字符串进行字符串拼接
+        StringBuilder result = new StringBuilder();
+        String line = null;
+        //每次读取一行
+        while ((line = reader.readLine()) != null) {
+            if (result.length() > 0) {
+                //长度大于0,则在尾部添加换行符,确保读取到得数据独立一行
+                result.append("\n");
+            }
+            //将读取到得结果追加到result对象得末尾
+            result.append(line);
+        }
+        //将最后结果以字符串结果返回
+        return result.toString();
+    }
+
+    /**
+     * 数据解密
+     *
+     * @param body
+     * @return
+     * @throws Exception
+     */
+    private String decryptData(String body) throws Exception {
+        //将请求体中得数据转换成json格式
+        JSONObject resultObject = JSON.parseObject(body);
+        //从resultObject这个json对象获取键名为"resource"的子对象内容
+        JSONObject resource = resultObject.getJSONObject("resource");
+        //从resource这个子对象中获取键名为"ciphertext"加密字符串,并存储在ciphertext这个字符串变量中
+        String ciphertext = resource.getString("ciphertext");
+        //同上,根据不同键,获取相应的内容
+        String nonce = resource.getString("nonce");
+        String associatedData = resource.getString("associated_data");
+
+        //将从微信配置属性类中获取到的密钥以UTF8的格式转化成一个字节数组创建一个AesUtil工具类对象
+        AesUtil aesUtil = new AesUtil(weChatProperties.getApiV3Key().getBytes(StandardCharsets.UTF_8));
+        //密文解密
+        String plainText = aesUtil.decryptToString(associatedData.getBytes(StandardCharsets.UTF_8),
+                nonce.getBytes(StandardCharsets.UTF_8),
+                ciphertext);
+
+        //将解密后的明文字符串返回
+        return plainText;
+    }
+
+    /**
+     * 给微信响应
+     * @param response
+     */
+    private void responseToWeixin(HttpServletResponse response) throws Exception{
+        response.setStatus(200);
+        HashMap<Object, Object> map = new HashMap<>();
+        map.put("code", "SUCCESS");
+        map.put("message", "SUCCESS");
+        //设置响应头的格式,指定响应内容为JSON格式
+        response.setHeader("Content-type", ContentType.APPLICATION_JSON.toString());
+        //获取输出流,并将map集合转换成json格式,并在转换成utf8的字节数组,通过响应的输出流写入到响应中
+        response.getOutputStream().write(JSONUtils.toJSONString(map).getBytes(StandardCharsets.UTF_8));
+        //将缓冲区中的内容发出
+        response.flushBuffer();
+    }
+}
--- sky-server/src/main/java/com/sky/service/impl/OrderServiceImpl.java
+++ sky-server/src/main/java/com/sky/service/impl/OrderServiceImpl.java
@@ -1,26 +1,25 @@
 package com.sky.service.impl;
 
+import com.alibaba.fastjson.JSONObject;
 import com.sky.constant.MessageConstant;
 import com.sky.context.BaseContext;
+import com.sky.dto.OrdersPaymentDTO;
 import com.sky.dto.OrdersSubmitDTO;
-import com.sky.entity.AddressBook;
-import com.sky.entity.OrderDetail;
-import com.sky.entity.Orders;
-import com.sky.entity.ShoppingCart;
+import com.sky.entity.*;
 import com.sky.exception.AddressBookBusinessException;
 import com.sky.exception.OrderBusinessException;
 import com.sky.exception.ShoppingCartBusinessException;
-import com.sky.mapper.AddressBookMapper;
-import com.sky.mapper.OrderDetailMapper;
-import com.sky.mapper.OrderMapper;
-import com.sky.mapper.ShoppingCartMapper;
+import com.sky.mapper.*;
 import com.sky.service.OrderService;
+import com.sky.utils.WeChatPayUtil;
+import com.sky.vo.OrderPaymentVO;
 import com.sky.vo.OrderSubmitVO;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.math.BigDecimal;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
@@ -41,6 +40,10 @@ public class OrderServiceImpl implements OrderService {
     private AddressBookMapper addressBookMapper;
     @Autowired
     private ShoppingCartMapper shoppingCartMapper;
+    @Autowired
+    private WeChatPayUtil weChatPayUtil;
+    @Autowired
+    private UserMapper userMapper;
 
     /**
      * 用户下单
@@ -99,5 +102,56 @@ public class OrderServiceImpl implements OrderService {
         orderSubmitVO.setOrderNumber(orders.getNumber());
         orderSubmitVO.setOrderAmount(orders.getAmount());
         return orderSubmitVO;
+    }
+
+    /**
+     * 订单支付
+     * @param ordersPaymentDTO
+     * @return
+     */
+    @Override
+    public OrderPaymentVO payment(OrdersPaymentDTO ordersPaymentDTO) throws Exception {
+        // 获取当前登录用户id
+        // Long userId = BaseContext.getCurrentId();
+        // User user = userMapper.getById(userId);
+
+        //调用微信支付接口，生成预支付交易单 ==========>跳过微信支付,因为无营业牌照
+        /*JSONObject jsonObject = weChatPayUtil.pay(
+                ordersPaymentDTO.getOrderNumber(), //商户订单号
+                new BigDecimal(0.01), //支付金额，单位 元
+                "苍穹外卖订单", //商品描述
+                user.getOpenid() //微信用户的openid
+        );
+
+        if (jsonObject.getString("code") != null && jsonObject.getString("code").equals("ORDERPAID")) {
+            throw new OrderBusinessException("该订单已支付");
+        }*/
+        // JSONObject jsonObject = new JSONObject();
+        //将json对象转换成类型为OrderPaymentVO的java对象"vo"
+        // OrderPaymentVO vo = jsonObject.toJavaObject(OrderPaymentVO.class);
+        //将从json对象中获取的统一下单接口设置为"vo"的统一下单接口返回的 prepay_id 参数值
+        // vo.setPackageStr(jsonObject.getString("package"));
+        // return vo;
+
+        //获取订单号作为参数调用paySuccess方法,模拟微信支付已完成
+        paySuccess(ordersPaymentDTO.getOrderNumber());
+        //返回值直接返回空值
+        return null;
+    }
+
+    @Override
+    public void paySuccess(String outTradeNo) {
+        // 根据订单号查询订单
+        Orders ordersDB = orderMapper.getByNumber(outTradeNo);
+
+        // 根据订单id更新订单的状态、支付方式、支付状态、结账时间
+        Orders orders = Orders.builder()
+                .id(ordersDB.getId())
+                .status(Orders.TO_BE_CONFIRMED)
+                .payStatus(Orders.PAID)
+                .checkoutTime(LocalDateTime.now())
+                .build();
+
+        orderMapper.update(orders);
     }
 }
